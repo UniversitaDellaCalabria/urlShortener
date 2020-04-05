076e73692fcfcd57254673fd2d6ea4d2afd04ca9
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-04-05 14:53:04 +0200

    Result page

--- tinyurl/urlshortener/views.py
+++ tinyurl/urlshortener/views.py
@@ -1,3 +1,5 @@
+import short_url
+
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render
@@ -11,7 +13,7 @@ def urlshortener(request):
     # test message :)
     #messages.add_message(request, messages.ERROR,
                                  #_('The URL you have inserted is not valid.'))
-    
+    urlsh = None
     if request.method == 'GET':
         form = UrlShortenerForm()
 
@@ -20,9 +22,18 @@ def urlshortener(request):
         if not form.is_valid():
             messages.add_message(request, messages.ERROR,
                                  _('The URL you have inserted is not valid.'))
+        else:
+            entry = dict(original_url=form.cleaned_data['url'],
+                         user_id=request.user if request.user.is_authenticated else None)
+            
+            urlsh = UrlShortener.objects.create(**entry)
+            urlsh.shorten_url = short_url.encode_url(urlsh.pk)
+            urlsh.save()
+    
     context = dict(
         project_name='Url Shortener',
         form = form,
-        delta_days = DELTA_DAYS
+        delta_days = DELTA_DAYS,
+        urlsh = urlsh
     )
     return render(request, 'urlshortener.html', context)
